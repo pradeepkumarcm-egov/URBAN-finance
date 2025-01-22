88281cd0f88922143b256b15391fb5e39b22a5b6
mukund-egov <mukund.tripathi@egovernments.org>
2025-01-22 17:58:29 +0530

    Fixed survey api issue from frontend

--- frontend/micro-ui/web/micro-ui-internals/packages/libraries/src/hooks/surveys/useSurveyInbox.js
+++ frontend/micro-ui/web/micro-ui-internals/packages/libraries/src/hooks/surveys/useSurveyInbox.js
@@ -3,31 +3,33 @@ import { useMutation, useQuery } from "react-query";
 /* import { isObject, isObjectLike } from "lodash"; */
 
 const useSearch = (filters, config) => {
-    const { filterForm, searchForm, tableForm } = filters
-    const { status } = filterForm
-    const { title, tenantIds, postedBy } = searchForm
-    const { sortBy, limit, offset, sortOrder } = tableForm;
-    const validTenantId = typeof tenantIds === 'object' ? tenantIds.code : tenantIds;
-    const validStatus = typeof status === 'object' ? status.code : status;
+  const { filterForm, searchForm, tableForm } = filters;
+  const { status } = filterForm;
+  const { title, tenantIds, postedBy } = searchForm;
+  const { sortBy, limit, offset, sortOrder } = tableForm;
+  const validTenantId = typeof tenantIds === "object" ? tenantIds.code : tenantIds;
+  const validStatus = typeof status === "object" ? status.code : status;
 
-    const finalFilters = {
-        tenantIds: validTenantId,
-        status: validStatus === "ALL" ? "" : validStatus,
-        title,
-        postedBy,
-        limit,
-        offset
+  const finalFilters = {
+    tenantId: validTenantId,
+    status: validStatus === "ALL" ? "" : validStatus,
+    title,
+    postedBy,
+    limit,
+    offset,
+  };
+
+  //clearing out empty string params from payload
+  Object.keys(finalFilters).forEach((key) => {
+    if (finalFilters[key] === "") {
+      delete finalFilters[key];
     }
+  });
 
-    //clearing out empty string params from payload
-    Object.keys(finalFilters).forEach(key => {
-        if (finalFilters[key] === '') {
-            delete finalFilters[key];
-        }
-    });
-
-
-    return useQuery(["search_surveys", title, tenantIds, postedBy, status, offset, limit], () => Surveys.search(finalFilters), { ...config, refetchInterval: 6000 });
+  return useQuery(["search_surveys", title, tenantIds, postedBy, status, offset, limit], () => Surveys.search(finalFilters), {
+    ...config,
+    refetchInterval: 6000,
+  });
 };
 
 export default useSearch;
