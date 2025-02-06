09e6545711dd7251f29ce84980d6dfee0b5b1baa
Palak Garg <86659286+palak-egov@users.noreply.github.com>
2025-02-06 15:32:12 +0530

    Update SurveyRepository.java

--- utilities/egov-survey-services/src/main/java/org/egov/egovsurveyservices/repository/SurveyRepository.java
+++ utilities/egov-survey-services/src/main/java/org/egov/egovsurveyservices/repository/SurveyRepository.java
@@ -84,9 +84,6 @@ public class SurveyRepository {
     public Integer fetchTotalSurveyCount(SurveySearchCriteria criteria) {
         List<Object> preparedStmtList = new ArrayList<>();
 
-        if(CollectionUtils.isEmpty(criteria.getTenantIds()) && ObjectUtils.isEmpty(criteria.getUuid()))
-            return 0;
-
         // Omit pagination in case of count
         criteria.setIsCountCall(Boolean.TRUE);
         String query = surveyQueryBuilder.getSurveyCountQuery(criteria, preparedStmtList);
