a20c549111c6f484be34cf578a8d1397b9078dca
salaudeenegov <salaudeen.n@egovernments.org>
2025-06-19 17:33:39 +0530

    fixed the small bug in import

--- frontend/micro-ui/web/micro-ui-internals/packages/modules/birth/src/Module.js
+++ frontend/micro-ui/web/micro-ui-internals/packages/modules/birth/src/Module.js
@@ -1,3 +1,4 @@
+import React from "react";
 import { CitizenHomeCard, FormComposerV2, Loader } from "@egovernments/digit-ui-react-components";
 import { BirthConfig } from "./pages/employee/createBirth/config/BirthConfig";
 
