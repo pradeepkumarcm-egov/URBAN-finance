07f2bdfe7d8fc69ddf0d26e78d6aadec9617b2fe
kams <kamalesh.m@egovernments.org>
2025-06-19 11:36:59 +0530

    Revert "changed the null values for pincode and builidingName to """
    This reverts commit cdffda64deb30339aa5ed9382415650850a75b32.

--- frontend/mono-ui/web/rainmaker/dev-packages/egov-firenoc-dev/src/ui-config/screens/specs/fire-noc/applyResource/propertyLocationDetails.js
+++ frontend/mono-ui/web/rainmaker/dev-packages/egov-firenoc-dev/src/ui-config/screens/specs/fire-noc/applyResource/propertyLocationDetails.js
@@ -173,14 +173,6 @@ const getDetailsFromProperty = async (state, dispatch) => {
             )
           );
         } else {
-          if (payload.Properties[0].address) {
-            if (payload.Properties[0].address.pincode === null) {
-              payload.Properties[0].address.pincode = "";              
-            }
-            if (payload.Properties[0].address.buildingName === null) {
-              payload.Properties[0].address.buildingName = "";              
-            }
-          }
           dispatch(
             handleField(
               "apply",
