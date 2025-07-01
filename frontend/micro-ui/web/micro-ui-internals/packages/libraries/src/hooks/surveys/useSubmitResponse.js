b4263198387df115689acc9ac1605622e6078e0c
Admin <admin@Admins-MacBook-Air.local>
2025-07-01 19:13:34 +0530

    Removed unwanted modules

--- frontend/micro-ui/web/micro-ui-internals/packages/libraries/src/hooks/surveys/useSubmitResponse.js
+++
@@ -1,9 +0,0 @@
-import { Surveys } from "../../services/elements/Surveys";
-import { useMutation } from "react-query";
-
-const useSubmitResponse = (filters, config) => {
-  const params = { tenantId: Digit.ULBService.getCurrentTenantId() };
-  return useMutation((filters) => Surveys.submitResponse(filters, params));
-};
-
-export default useSubmitResponse;
